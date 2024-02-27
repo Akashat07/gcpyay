508415e299532dd2304bd90a3a2780417d7284da
Akashat07 <143158244+Akashat07@users.noreply.github.com>
2024-02-27 14:09:34 +0530

    Update gcpyay.py

--- gcpyay.py
+++ gcpyay.py
@@ -1,12 +1,3 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
-class MainHandler(webapp.RequestHandler):
- def get(self):
- self.response.out.write('SatShriAkal! Himanshu Pra \n Tuhadi UID hai -
-21BCS3138 ')
-def main():
- application = webapp.WSGIApplication([('/', MainHandler)],
- debug=True)
- util.run_wsgi_app(application)
-if __name__ == '__main__':
- main()
+# This program prints Hello, world!
+
+print('Hello, world!')
